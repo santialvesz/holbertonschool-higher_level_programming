278bfdf48bdbd4f186497578f661f1e4ba5f7229
santialvesz <santialvesdsc@gmail.com>
2022-12-21 14:55:26 -0300

    All tasks

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Rectangle = require('./5-square');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) c = 'X';
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+};
