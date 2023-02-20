252e6001a839dcc96c2af459c2e87b0f89fd9c9f
santialvesz <santialvesdsc@gmail.com>
2023-02-20 11:37:32 -0300

    Fix

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
+const argv = require('process').argv;
 
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-};
+if (isNaN(argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', argv[2]);
+}
